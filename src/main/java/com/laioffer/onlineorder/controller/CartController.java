0b06368392723090f78cf223b560528e31c9bf5f
AnyanXie01 <anyanxie01@berkeley.edu>
2024-05-24 17:04:33 +0800

    cart controller

--- src/main/java/com/laioffer/onlineorder/controller/CartController.java
+++ src/main/java/com/laioffer/onlineorder/controller/CartController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CartController {
 
-    // daily commit:5/24(2)
+    // daily commit:5/24(3)
     private final CartService cartService;
     private final CustomerService customerService;
     public CartController(CartService cartService, CustomerService customerService) {
